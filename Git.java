00708c22271102ac023a2f5d589be2b1a3253d69
Kyara Zhou <kzhou1@hwemail.com>
2024-09-26 10:15:00 -0700

    create blob for trees

--- Git.java
+++ Git.java
@@ -10,50 +10,102 @@ import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.zip.DeflaterOutputStream;
+import java.math.BigInteger;
 
 public class Git {
-    File gitDirectory = new File ("git");
-    File objectsDirectory = new File ("git/objects");
+    File gitDirectory = new File("git");
+    File objectsDirectory = new File("git/objects");
     String indexFileName = "index";
-    File indexFile = new File (gitDirectory, indexFileName);
+    File indexFile = new File(gitDirectory, indexFileName);
     boolean toZip;
 
-    public Git (boolean toZip) throws IOException {
+    // blame Aviv if everything looks messy >:(
+    // I had to manually add in all the comments - Kyara
+
+    public Git(boolean toZip) throws IOException {
         this.toZip = toZip;
         if (!gitDirectory.exists()) {
             gitDirectory.mkdir();
             objectsDirectory.mkdirs();
             indexFile.createNewFile();
-        }
-        else if (objectsDirectory.exists() && !indexFile.exists()){
+        } else if (objectsDirectory.exists() && !indexFile.exists()) {
             indexFile.createNewFile();
-        }
-        else if (indexFile.exists() && !objectsDirectory.exists()) {
+        } else if (indexFile.exists() && !objectsDirectory.exists()) {
             objectsDirectory.mkdirs();
-        }
-        else if (!indexFile.exists() && !objectsDirectory.exists()) {
+        } else if (!indexFile.exists() && !objectsDirectory.exists()) {
             indexFile.createNewFile();
             objectsDirectory.mkdirs();
-        }
-        else {
+        } else {
             System.out.println("Git Repository already exists");
         }
 
     }
 
-    public void compressData (String fileName) throws IOException {
+    public void compressData(String fileName) throws IOException {
         FileInputStream fis = new FileInputStream(fileName);
         FileOutputStream fos = new FileOutputStream("compressedFile.zip");
         DeflaterOutputStream dos = new DeflaterOutputStream(fos);
-        int data; 
-        while ((data=fis.read())!=-1) 
-        { 
-            dos.write(data); 
-        } 
-        fis.close(); 
-        dos.close(); 
+        int data;
+        while ((data = fis.read()) != -1) {
+            dos.write(data);
+        }
+        fis.close();
+        dos.close();
     }
 
+    // creates a blob.
+    // fileName is actually the file Path
+    public void makeBLOB(String fileName) throws IOException, NoSuchAlgorithmException {
+        File temp = new File(fileName);
+        StringBuilder fileContent = new StringBuilder();
+        // if not exist, then throw exceptions
+        if (!temp.exists()) {
+            throw new FileNotFoundException("the file doesn't exist");
+        } else if (temp.isDirectory()) {
+            // if is directory, then first list out the file, create a blob for each
+            // content, append a small index into a string, and hash that for the tree
+            File[] files = temp.listFiles();
+            StringBuilder sb = new StringBuilder();
+            for (File currFile : files) {
+                // for each currFile, make a blob
+                makeBLOB(currFile.getPath());
+                sb.append(currFile+"\n");
+            }
+            String treeHash = hashTree(sb);
+            // creates file for the tree
+            File file = new File("git/objects", treeHash);
+            file.createNewFile();
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+            bw.append(sb.toString());
+
+            fileContent.append("tree " + treeHash + " " + new File(fileName).getPath());
+            bw.close();
+
+        } else {
+            // hashes a blob
+            String hash = hashingFunction(fileName);
+            File file = new File("git/objects", hash);
+            file.createNewFile();
+            fileWriter(fileName, file);
+            fileContent.append("blob " + hash + " " + new File(fileName).getPath());
+        }
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(indexFile, true))) {
+            bw.append(fileContent.toString());
+            bw.newLine();
+        }
+
+    }
+
+    // hashes the small list of files for trees
+    private String hashTree(StringBuilder sb) throws IOException, NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-1");
+        String content = sb.toString();
+        digest.update(content.getBytes("UTF-8"));
+        BigInteger temp = new BigInteger(1, digest.digest());
+        return temp.toString(16);
+    }
+
+    // generates the hashed name using file content (blob only)
     public String hashingFunction(String fileName) throws FileNotFoundException, NoSuchAlgorithmException, IOException {
         if (toZip) {
             compressData(fileName);
@@ -61,7 +113,6 @@ public class Git {
         }
         File file = new File(fileName);
         MessageDigest digest = MessageDigest.getInstance("SHA-1");
-
         try (FileInputStream fis = new FileInputStream(file)) {
             byte[] byteArray = new byte[1024];
             int bytesRead;
@@ -69,7 +120,7 @@ public class Git {
                 digest.update(byteArray, 0, bytesRead);
             }
         }
-        
+
         byte[] hash = digest.digest();
         StringBuilder hexString = new StringBuilder();
         for (byte b : hash) {
@@ -82,26 +133,14 @@ public class Git {
         return hexString.toString();
     }
 
-    public void fileWriter (String toRead, File toWrite) throws IOException {
-        BufferedWriter bw = new BufferedWriter (new FileWriter (toWrite));
+    // creates a file with given content(blob only)
+    public void fileWriter(String toRead, File toWrite) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(toWrite));
         BufferedReader br = new BufferedReader(new FileReader(toRead));
         while (br.ready()) {
             bw.append((char) br.read());
         }
         br.close();
-        bw.close(); 
+        bw.close();
     }
-
-    public void makeBLOB(String fileName) throws IOException, NoSuchAlgorithmException {
-        String hash = hashingFunction(fileName);
-        File file = new File("git/objects", hash);
-        file.createNewFile();
-        fileWriter(fileName, file);
-        String toAppend = hash + " " + new File(fileName).getName();
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(indexFile, true))) {
-            bw.append(toAppend);
-            bw.newLine(); 
-        }
-    }
-
 }
---
+++ simpleTester.java
@@ -0,0 +1,51 @@
+import java.security.NoSuchAlgorithmException;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class simpleTester {
+    // I AM SO TIRED OF AVIV"S ANNOYING AF TESTERS - Kyara
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        
+        // testing the mk blob with trees
+        Git myGit = new Git(false);
+        mkTesters();
+        //myGit.makeBLOB("./hm"); // testing file not found exception
+        myGit.makeBLOB("./testingFolder");
+
+    }
+
+
+    // creates testers for testing
+    private static void mkTesters() throws IOException, NoSuchAlgorithmException {
+        File testingFolder = new File("./testingFolder");
+        testingFolder.mkdir();
+        File indexFile = new File(testingFolder, "item");
+        BufferedWriter tf = new BufferedWriter(new FileWriter(indexFile));
+        tf.write("why is this so confusing");
+        File testingFolder2 = new File(testingFolder, "testingFolder2");
+        testingFolder2.mkdir();
+        File test2 = new File(testingFolder2, "item2");
+        BufferedWriter tf2 = new BufferedWriter(new FileWriter(test2));
+        tf2.write("cry");
+        tf.close();
+        tf2.close();
+
+        // File file1 = new File("file1.txt");
+        // File file2 = new File("file2.txt");
+        // File file3 = new File("file3.txt");
+        // BufferedWriter bw1 = new BufferedWriter(new FileWriter(file1));
+        // bw1.write("test1");
+        // bw1.close();
+        // BufferedWriter bw2 = new BufferedWriter(new FileWriter(file2));
+        // bw2.write("test2");
+        // bw2.close();
+        // BufferedWriter bw3 = new BufferedWriter(new FileWriter(file3));
+        // bw3.write("test3");
+        // bw3.close();
+    }
+
+}
